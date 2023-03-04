2d342ac0781d308f881812342ffb64c7f36ac272
Mohammad Umair <umairmohd@fb.com>
2023-03-04 13:26:42 -0700

    add UI inside the plugin toolwindow
    Signed-off-by: Mohammad Umair <umairmohd@fb.com>

--- plugin/src/main/kotlin/zk/bnb/android/plugin/extensions/MainToolWindowFactory.kt
+++ plugin/src/main/kotlin/zk/bnb/android/plugin/extensions/MainToolWindowFactory.kt
@@ -3,9 +3,11 @@ package zk.bnb.android.plugin.extensions
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.ui.content.Content
-import com.intellij.ui.content.ContentFactory
+import com.intellij.util.ui.JBUI
+import javax.swing.JButton
+import javax.swing.JLabel
 import javax.swing.JPanel
+import javax.swing.border.EmptyBorder
 
 class MainToolWindowFactory: ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
@@ -14,9 +16,24 @@ class MainToolWindowFactory: ToolWindowFactory {
 //        val content: Content = contentFactory.createContent(myToolWindow.content, "", false)
 //        toolWindow.contentManager.addContent(content)
 
+        val html = "<html>" +
+                "<h1>ZkBNB</h1><br/>" +
+                "<h2>Scaling breakthrough for BNB Chain</h2><br/>" +
+                "<p>Click the following button to add the ZkBNB Android SDK to your project.</p>" +
+                "</html>"
+
         // Create Jpanel
         // Create Jpanel
         val myPanel = JPanel()
+        myPanel.border = JBUI.Borders.empty(40, 40)
+        val mainLabel = JLabel(html)
+        myPanel.add(mainLabel)
+
+        val b = JButton("Click Here")
+        myPanel.add(b)
+
+//        val subLabel = JLabel("<html><br></html>")
+//        myPanel.add(subLabel)
 
         // Add Jpanel to toolWindow
 
